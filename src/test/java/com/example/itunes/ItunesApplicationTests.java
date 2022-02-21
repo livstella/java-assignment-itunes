6d1caa86afbb99d44765d5c8b740c396f59754d4
Liv Reinhold <livreinhold@gmail.com>
2022-02-21 13:55:20 +0100

    Initital commit

---
+++ src/test/java/com/example/itunes/ItunesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itunes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItunesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
