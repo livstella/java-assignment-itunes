07283c22127fd59c07d7e5e2f4d1096aa044eb74
Liv Reinhold <livreinhold@gmail.com>
2022-02-22 15:16:22 +0100

    added a test query with the db url as the absolute path

---
+++ src/main/java/com/example/itunes/ConnectionManager.java
@@ -0,0 +1,34 @@
+package com.example.itunes;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+    static final String URL = "jdbc:sqlite:C:\\Users\\LReinh\\Desktop\\Projects\\Java\\Itunes-project\\Itunes\\src\\main\\resources\\Chinook_Sqlite.sqlite";
+    static private ConnectionManager instance;
+    private Connection connection;
+
+    static ConnectionManager getInstance(){
+      if (instance == null){
+          instance = new ConnectionManager();
+      }
+      return instance;
+    }
+
+    private ConnectionManager(){
+        try {
+            connection = DriverManager.getConnection(URL);
+        }catch (SQLException sqe){
+            sqe.printStackTrace();
+            System.exit(-1);
+        }
+
+    }
+
+    public Connection getConnection(){
+        return connection;
+    }
+
+
+}
--- src/main/java/com/example/itunes/ItunesApplication.java
+++ src/main/java/com/example/itunes/ItunesApplication.java
@@ -2,12 +2,51 @@ package com.example.itunes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+import java.sql.*;
+import java.util.List;
+
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@RestController
 public class ItunesApplication {
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(ItunesApplication.class, args);
+
+        Connection conn=ConnectionManager.getInstance().getConnection();
+
+
+
+
+        try {
+            PreparedStatement preparedStatement =conn.prepareStatement("SELECT Name  from main.Genre WHERE GenreId=1");
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()){
+
+                System.out.printf(resultSet.getString("Name"));
+            }
+
+            conn.close();
+        }catch (SQLException sqe){
+            sqe.printStackTrace();
+            System.exit(-1);
+        }
+
+
+
+
+    }
+
+    @GetMapping
+    public List<String> test(){
+        return List.of("Hello, World");
     }
 
 }
